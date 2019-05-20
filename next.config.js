45e07fcbbf688d3d023150c07bd6bbd232d58e92
mindock <parksy8189@gmail.com>
2019-05-20 13:05:50 +0900

    fix: 배포환경에서 실행안되는 문제 해결
    1. assetRelocator 사용

--- next.config.js
+++ next.config.js
@@ -2,27 +2,32 @@ const withCSS = require('@zeit/next-css');
 const withTypescript = require('@zeit/next-typescript');
 const ForkTsCheckerWebapckPlugin = require('fork-ts-checker-webpack-plugin');
 const loaderUtils = require('loader-utils');
+const withAssetRelocator = require('./assetRelocator');
 
-module.exports = withTypescript(
-  withCSS({
-    cssModules: true,
-    cssLoaderOptions: {
-      importLoaders: 1,
-      localIdentName: '[path]-[local]-[hash:base64:4]',
-      getLocalIdent: (context, localIdentName, localName, options) => {
-        if (context.resourcePath.includes('/node_modules')) {
-          return localName;
-        }
-        return localName + loaderUtils.interpolateName(context, '-[hash:base64:4]', { content: context.resourcePath });
+module.exports = withAssetRelocator(
+  withTypescript(
+    withCSS({
+      cssModules: true,
+      cssLoaderOptions: {
+        importLoaders: 1,
+        localIdentName: '[path]-[local]-[hash:base64:4]',
+        getLocalIdent: (context, localIdentName, localName, options) => {
+          if (context.resourcePath.includes('/node_modules')) {
+            return localName;
+          }
+          return (
+            localName + loaderUtils.interpolateName(context, '-[hash:base64:4]', { content: context.resourcePath })
+          );
+        },
       },
-    },
-    target: 'serverless',
-    webpack(config, { dev, isServer }) {
-      config.devtool = 'eval-source-map';
-      if (dev && isServer) {
-        config.plugins.push(new ForkTsCheckerWebapckPlugin());
-      }
-      return config;
-    },
-  }),
+      target: 'serverless',
+      webpack(config, { dev, isServer }) {
+        config.devtool = 'eval-source-map';
+        if (dev && isServer) {
+          config.plugins.push(new ForkTsCheckerWebapckPlugin());
+        }
+        return config;
+      },
+    }),
+  ),
 );
